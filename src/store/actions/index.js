9854f74ca2599d6a99dd4d6ed802b0e925a4d2f8
Jean Domingues <jeandomingues752@gmail.com>
2021-03-31 21:30:27 -0300

    criando novos users

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -3,6 +3,7 @@ import dados from '../../dados.json';
 import { setItemStorage } from '../../utils/localStorage'
 
 export const authValidation = (dispatch, { email, password}) => {
+  // Desconsiderar o sistema de autentificação, é apenas uma simulação.
   email = email.trim();
   password = Number(password);
   const { password: senha, ...rest} = dados[email];
